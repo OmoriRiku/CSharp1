260a66443772cfd7ab3d7f0e1b553c48be7d562d
OmoriRiku <oomori3321@gmail.com>
2024-07-09 23:49:32 +0900

    三回目のコミット

---
+++ sample/Sample/Program.cs
@@ -0,0 +1,25 @@
+﻿//  if文の例(キーボードが入力された値が10であればメッセージを表示する。)
+
+using System;
+
+namespace Sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  キーボードから入力した値を変数lineへ代入する。
+            Console.Write("数値を入力してください(10)：");
+            var line = Console.ReadLine();
+
+            //  入力されたString型の値をint型へ変換する
+            var num = int.Parse(line);
+
+            //  10が入力されればメッセージを表示する。
+            if (num == 10)
+            {
+                Console.WriteLine("10と入力されました。");
+            }
+        }
+    }
+}
